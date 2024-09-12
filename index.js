294c8e20c2b29a59f3e1b499a2481c1f9e7a6139
iggonicollas <iggonicollas@gmail.com>
2024-09-12 14:42:59 -0300

    inicia menu applicacao

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-const start = () => {
+const { select } = require("@inquirer/prompts")
+
+const start = async () => {
 
     while(true){
-        let opcao = "sair"
+    
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+
         switch(opcao) {
             case "cadastrar":
                 console.log("vamos cadastrar")
